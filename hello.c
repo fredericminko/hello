c0188f2366416ced3b674d6dd014c3619f7a9fcf
frederic herbert bekoo minko <fredericminko2@huskers.edu>
2021-08-26 15:14:42 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Frederic herbert bekoo minko
+ * 8/24/2021
+ * 
+ * hello
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello World!\n");
+     
+     return 0;
+ }
+ 
---
+++ info.md
@@ -0,0 +1,1 @@
+frederic herbert bekoo minko, fredericminko2@huskers.unl.edu
