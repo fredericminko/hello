5d11aefdadf65edd91ce5e288b4554e275ff62ad
Mac DePriest <mdepriest2@huskers.unl.edu>
2021-08-27 18:54:42 +0000

    i put jokes in the code

--- hello.c
+++ hello.c
@@ -1,21 +1,23 @@
 /**
  * Frederic herbert bekoo minko
  * 8/24/2021
- * 
+ *
  * hello
- * 
+ *
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
      printf("Hello World, l am Frederic minko!\n");
      printf("my major is electrical engineering!\n");
-   
-     
+
+     printf("My name is Mac DePriest but you can call me tonight\n");
+     printf("I am a mathematics major, a mathemajor if you will\n");
+
+
      return 0;
  }
 
 
- 
