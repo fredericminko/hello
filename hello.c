f12a531ea41fda7bf4c2c3fddb7b9198cf6d9361
frederic minko <fbekoominko2@huskers.unl.edu>
2022-08-29 21:28:22 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic hello world program
+ */
+
+ int main (int argc,char **argv) {
+
+     printf("hello, l'm frederic minko!\);
+     printf("Major is electrical engineering!\n);
+
+     return 0;
+ }
